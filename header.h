e8a1289e0e3d89637fbab5584a700415d5c69b25
kelvinkioi <kelvinkioi.101k@gmail.com>
2023-11-30 18:20:43 +0300

    adding rendering functions to the header file

--- header.h
+++ header.h
@@ -7,6 +7,7 @@
 #include <limits.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include "upng.h"
 
 #define PI 3.14159265
 #define TILE_SIZE 64
@@ -23,10 +24,11 @@ typedef uint32_t colour_t;
 /* Functions-variables-structs for textures */
 
 /**
- * struct texture_s - struct for the textures
+ * struct texture_k - struct for the textures
  * @width: texture width
  * @height: texture height
  * @texture_buffer: pointer to texture buffer
+ * @upng_texture: pointer to the upng buffer
  *
  */
 typedef struct texture_k
@@ -34,11 +36,14 @@ typedef struct texture_k
 	int width;
 	int height;
 	colour_t *texture_buffer;
+	upng_t *upng_texture;
 } texture_t;
 
 texture_t wall_textures[NUM_TEXTURES];
 
 void change_colour_intensity(colour_t *colour, float factor);
 void render_floor(int bottomwall_pixel, colour_t *texture_colour, int x);
+void render_ceil(int topwall_pixel, colour_t texture_colour, int x);
+void render_wall(void);
 
 #endif /*HEADER_H*/
